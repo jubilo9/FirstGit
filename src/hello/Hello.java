d28818ffcf097f3b160fca1586146fc20a9242b4
g13944sm <g13944sm@s30421.tsuda.ac.jp>
2015-04-10 11:18:27 +0900

    HelloWorldです。

---
+++ src/hello/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author g13944sm
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+        System.out.println("こんにちは世界");
+        System.out.println(10+5);
+    }
+}
